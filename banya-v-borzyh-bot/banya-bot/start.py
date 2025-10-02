8ea0659e6b7f9db5d5a089b870806dde6ccb2561
keybody65 <popov27051971@gmail.com>
2025-10-02 03:45:23 +0300

    Add files

---
+++ banya-v-borzyh-bot/banya-bot/start.py
@@ -0,0 +1,30 @@
+from aiogram import Bot, Dispatcher
+import asyncio
+import os
+from dotenv import load_dotenv
+import logging
+from app.handlers import router
+
+load_dotenv()
+
+token = os.getenv("BOT_TOKEN")
+
+async def main(token):
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - [%(levelname)s] - '  # логирование нужно для отображения результата работы хендлера: is handled / is not handled
+                               '%(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s')
+
+    bot = Bot(token)
+    dp = Dispatcher()
+
+    dp.include_router(router)
+
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main(token=token))
+    
